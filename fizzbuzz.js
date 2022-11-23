b6f8765d580045cf4d5dadc218002c3b7a5e41be
Gustavo Fernandes Lima <gustaavofernandeslima@hotmail.com>
2022-11-23 16:21:03 -0300

    Comentários adicionados a code base

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,17 +1,25 @@
 for (var i = 1; i <= 100; i++) {
    
+    //Valida primeiro se a variável i é múltiplo de 3 e 5 simultaneamente e a imprime no console caso seja
+
     if ( (i % 3 == 0) && (i % 5 == 0) ) {
     console.log("FizzBuzz");
     }
   
+    //Valida se a variável i é múltiplo de 3 apenas e a imprime no console caso seja 
+
     if ( (i % 3 == 0) && (i % 5 != 0)) {
     console.log("Fizz");
     }
-    
+
+    //Valida se a variável i é múltiplo de 5 apenas e a imprime no console caso seja 
+
      if ( (i % 5 == 0) && (i % 3 != 0)) {
     console.log("Buzz");
     }
     
+    //Valida se a variável i não atende a nenhuma das condições e imprime caso não atenda 
+
     if ( (i % 3 != 0) && (i % 5 != 0) ) {
     console.log(i);
     }
